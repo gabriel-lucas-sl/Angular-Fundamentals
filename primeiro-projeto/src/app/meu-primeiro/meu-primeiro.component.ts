2dc8ea839c600766ba70c430c8f9ba7d16f64a02
CAPCO\GSLB <gabriel.silva@capco.com>
2021-01-06 08:03:50 -0300

    Atualização módulo 1

---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,10 @@
+import { literal } from "@angular/compiler/src/output/output_ast";
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro component com Angular 2!</p>
+    `
+})
+export class MeuPrimeiroComponent {}
